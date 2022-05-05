341a53301533477fbe906ab281e0b9389307b1d3
yuudj <yuudj@hotmail.com>
2022-05-05 00:28:02 -0300

    app.component.spec.ts Non exitting property check removed

--- projects/examples/src/app/app.component.spec.ts
+++ projects/examples/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'examples'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('examples');
   });
 
   it('should render title', () => {
