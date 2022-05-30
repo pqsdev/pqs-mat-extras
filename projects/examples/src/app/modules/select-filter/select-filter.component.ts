ba325701b61ca49f8ba93ce67fe0d3efa41b2244
yuudj <yuudj@hotmail.com>
2022-05-30 17:01:20 -0300

    InifiniteScroll Example fix

--- projects/examples/src/app/modules/select-filter/select-filter.component.ts
+++ projects/examples/src/app/modules/select-filter/select-filter.component.ts
@@ -73,7 +73,7 @@ export class SelectFilterComponent implements OnInit, AfterViewInit, OnDestroy {
       });
   }
   public getNextBatch() {
-    this.productDataSource.page++;
+    this.productDataSource2.page++;
   }
   // -----------------------------------------------------------------------------------------------------
   // @ Lifecycle hooks
